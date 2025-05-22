f5b77e330b8099addd682916b8936915bb0c0193
Seonjin <ebngk0@gmail.com>
2025-05-22 14:12:42 +0900

    🐛 bugfix: Bug fix that will close the toggle when you erase your favorites

--- src/typescript/toggle-menu.ts
+++ src/typescript/toggle-menu.ts
@@ -223,8 +223,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const deleteBtn = document.createElement('button');
     deleteBtn.setAttribute('type', 'button');
-    deleteBtn.setAttribute('class', 'cursor-pointer');
-    deleteBtn.setAttribute('class', 'delete-btn');
+    deleteBtn.setAttribute('class', 'cursor-pointer delete-btn');
     deleteBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-gray-400 hover:text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /> </svg>`;
 
     deleteBtn.addEventListener('click', event => {
